0cb5d784e33dc60a9716dd5231401c779dce3486
Carlos <carloscastro96@live.com.pt>
2026-07-08 02:38:32 +0100

    PaddingGeneralFix

--- components/ObjetivosSection.tsx
+++ components/ObjetivosSection.tsx
@@ -6,7 +6,7 @@ export default function ObjetivosSection() {
   return (
     <section id="sobre" className="flex flex-col items-center justify-center min-h-screen pt-12 pb-22 md:py-30 gap-12 relative overflow-hidden">
       <div className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-black to-transparent z-10 pointer-events-none" />
-      <div className="text-left mb-4 md:mb-12 w-full px-3 md:pl-65 relative z-10">
+      <div className="text-left mb-4 md:mb-12 w-full px-3 pl-7 md:pl-65 relative z-10">
         <p className="text-white/90 uppercase tracking-widest text-3xl mb-2">Os nossos</p>
         <h2 className="text-4xl md:text-7xl font-bold text-white/90 mb-4">Objetivos</h2>
         <p className="text-white/50 text-lg md:text-xl max-w-2xl">
